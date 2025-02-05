2faf9411af5aa3fca18edf1753b5dca53a7c3944
nguyen-gia-khoi <giakhoi2004@gmail.com>
2025-02-05 17:09:28 +0700

    test#1

--- app/(tabs)/HomeScreen.tsx
+++ app/(tabs)/HomeScreen.tsx
@@ -17,7 +17,7 @@ const ThuChi = [
       {/* Header */}
       <View style={styles.header}>
         <Button title="Back to Login" onPress={() => {}} />
-        <Text style={styles.username}>Nha nhoi</Text>
+        <Text style={styles.username}>thỉn</Text>
       </View>
 
       {/* Bieu do  */}
