c9917701b33f7f9b936bc9243775a0c13f5afec2
nguyen-gia-khoi <giakhoi2004@gmail.com>
2025-03-18 06:12:21 +0700

    feat/newTransaction

---
+++ API/transactionAPI.ts
@@ -0,0 +1,31 @@
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import axios from "axios"; 
+
+const apiUrl = `${process.env.EXPO_PUBLIC_API_URL}/v1/transaction`;
+
+export const newTransaction = async ( type : string,amount: string,date: string,note?: string,category?: string ) => {
+ try {
+    const currentUser = auth().currentUser;
+    console.log("Current user:", currentUser);
+    if (!currentUser) {
+      throw new Error("User is not authenticated");
+    }
+    console.log("API URL:", apiUrl);
+
+    const response = await axios.post(apiUrl, {
+        uid: currentUser.uid,
+        type,
+        amount: parseFloat(amount),
+        date,
+        category: category || "Khác",
+        note: note || "",
+      });
+  
+      console.log("Transaction saved:", response.data);
+      return { success: true, message: response.data.message };
+    
+ } catch (error) {
+    console.error("Error saving transaction:", error);
+    
+ }
+}
